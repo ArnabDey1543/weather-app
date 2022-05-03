85feb42f462001208c175057303bdf549fefe1b3
ArnabDey1543 <arnabdey1543@gmail.com>
2022-05-03 22:29:33 +0530

    adding files

--- src/components/tempapp.js
+++ src/components/tempapp.js
@@ -1,37 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./css/style.css";
 
 const Tempapp = () => {
-
+  
     const [city, setCity] = useState(null);
-    const [search , setSearch] = useState("Mumbai");
+    const [search, setSearch] = useState("Mumbai");
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b5cc9664cf4a1f366328f678739c2ac9`
+            const response = await fetch(url);
+            // console.log(response);
+            const resJson = await response.json();
+            // console.log(resJson);
+            setCity(resJson.main);
+
+
+        };
+
+        fetchApi();
+    }, [search])
 
     return (
         <>
             <div className="box">
                 <div className="inputData">
-                    <input type="search" className="inputField" onChange={(event) => {
-
+                    <input type="search" value={search} className="inputField" onChange={(event) => {
+                        setSearch(event.target.value)
                     }} />
                 </div>
+                {!city ? (
+                    <p className="errorMsg">No Data Found</p>
+                ) : (
+                    <div>
 
+                        <div className="info">
+                            <h2 className="location">
+                                <i className="fa-solid fa-street-view"></i> {search}
+                            </h2>
+                            <h1 className="temp">
+                                {city.temp}°Cel
+                            </h1>
+                            <h3 className="tempmin_max">
+                                Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+                            </h3>
+                        </div>
 
-                <div className="info">
-                    <h2 className="location">
-                        <i class="fa-solid fa-street-view"></i> {city}
-                    </h2>
-                    <h1 className="temp">
-                        5.25° Cel
-                    </h1>
-                    <h3 className="tempmin_max">
-                        Min : 5.26° Cel | Max : 5.25° Cel
-                    </h3>
-                </div>
-
-                <div className="wave -one"></div>
-                <div className="wave -two"></div>
-                <div className="wave -three"></div>
+                        <div className="wave -one"></div>
+                        <div className="wave -two"></div>
+                        <div className="wave -three"></div>
+                    </div>
+                )}
             </div>
+
         </>
     )
 }
